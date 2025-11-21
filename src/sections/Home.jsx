f053b8bfc18e69a6a9ed44cd5a49c06e724e71a1
Yashiikaaaa <yashika@truestate.in>
2025-11-21 14:43:11 +0530

    Update main title from 'Prestige Green Brook' to 'Prestige Crystal Lawns'

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -60,7 +60,7 @@ export const Home = ({ openContactModal }) => {
               <div className="flex gap-4 flex-col w-full">
                 {/* Main Title */}
                 <h1 className="font-subheading text-3xl md:text-6xl font-semibold uppercase">
-                 Prestige Green Brook
+                 Prestige Crytal Lawns
                 </h1>
 
                 {/* Subtitle */}
